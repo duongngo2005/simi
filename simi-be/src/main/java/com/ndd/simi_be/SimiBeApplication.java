dffa4d0bd8de32bcfecd128dc2bd382e2f1f1f70
duongngo2005 <info.duongngo@gmail.com>
2026-07-02 19:16:13 +0700

    chore: initial project

---
+++ simi-be/src/main/java/com/ndd/simi_be/SimiBeApplication.java
@@ -0,0 +1,13 @@
+package com.ndd.simi_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimiBeApplication.class, args);
+	}
+
+}
---
+++ simi-be/src/test/java/com/ndd/simi_be/SimiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndd.simi_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
